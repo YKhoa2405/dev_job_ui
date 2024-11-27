fbca8234e0198461a2b81bfd387a5af72d4e2d41
YKhoa2405 <nykhoa2405@gmail.com>
2024-11-27 14:26:40 +0700

    commit

--- HeyJob/src/config/Key.jsx
+++
@@ -1,4 +0,0 @@
-export const client_id_api = "SC93qLAKpYHvP5G5tz0hOwuXOznK3jTnyte3YpKe"
-export const client_secret_api = "SvQzb8IX1MtX2wVfEUHGbrpjiK3H1CsYmqrgj32EMBu5fw9bAConICsLF6be9FRF7Dzd2fgAYnXJUoOBvuBqvKTXpY3TzNWVv3zTcZ9xfzvPEQtFXV1AHzuEqWhDmb9G"
-export const azuze_map_primary_key_api = "FaqJhLIckgPjz6XjhcxZfxsRukcyNyZhHJzRZ8H3eMlo4sISPeilJQQJ99AIACYeBjF6AxJHAAAgAZMPFtxq"
-export const clinet_id_google="239735618406-cm48vh19b18p843uju9anq2jtsnha7ek.apps.googleusercontent.com"
